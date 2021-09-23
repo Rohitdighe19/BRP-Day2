9416ae893566da656e8ac3e769ef7cbf599ff3c1
rohitdighe19 <rohitdighe47@gmail.com>
2021-09-23 12:54:26 +0530

    Added: Solution Double output program

--- Doubleout.java
+++ Doubleout.java
@@ -1,24 +1,23 @@
 import java.util.Scanner;
 
-public class Intopt {
+public class Doubleout {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int a,b,c,num1,num2,num3,num4;
+        double a,b,c,num1,num2,num3,num4;
         System.out.println("Enter Value of a , b , c :");
 
-        a = sc.nextInt();
-        b = sc.nextInt();
-        c = sc.nextInt();
+        a = sc.nextDouble();
+        b = sc.nextDouble();
+        c = sc.nextDouble();
 
         num1 = a + b * c;
         num2 = a * b + c;
         num3 = c + a / b;
         num4 = a % b + c;
-        
+
         System.out.println("num1 = a + b * c "+num1);
         System.out.println("num2 = a * b + c "+num2);
         System.out.println("num3 = c + a / b "+num3);
         System.out.println("num4 = a % b + c "+num4);
-
     }
 }
