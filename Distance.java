0c093b8144310d9c1f3932ed5ffcd9b41b94d80a
rohitdighe19 <rohitdighe47@gmail.com>
2021-09-23 13:09:22 +0530

    Added : Solution of Calculate the distance using math power fuction

---
+++ Distance.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Distance {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        double x ,y,distance;
+
+        System.out.println("Enter Arguments x : ");
+        x = sc.nextDouble();
+        System.out.println("Enter Arguments y : ");
+        y = sc.nextDouble();
+
+        distance = Math.sqrt(x*x + y*y);
+        System.out.println(distance);
+    }
+}
